2057f190bae5f144fcf1b13a2da41fad837a5fd2
Livio <66132299+Livio-cmd@users.noreply.github.com>
2023-08-21 20:02:05 +0200

    [ main ]

---
+++ script.js
@@ -0,0 +1,23 @@
+const audio = new Audio('../ressources/song/click.mp3');
+const body = document.querySelector('body');
+const box = document.getElementById('menu');
+
+function audioPlay() {
+    audio.play();
+    body.style.opacity = '0';
+    setTimeout(() => {
+        body.style.opacity = '1';
+    }, 200);
+}
+
+function closeMenu() {
+    box.click();
+}
+
+const images = document.querySelectorAll('img');
+
+images.forEach(img => {
+    if (img.src === 'https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png'){
+        img.remove();
+    }
+});
